16d5704e32b33f8f28a97d72f9f6c0ce82ffc212
gigijeskai <luigindaco@gmail.com>
2022-11-23 16:40:50 +0100

    simple commit

---
+++ final-project-fe/src/app/interfaces/annunci.ts
@@ -0,0 +1,12 @@
+export interface Annunci {
+  passeggeri: '5';
+  cambio: 'manuale';
+  alimentazione: 'diesel';
+  'Consumo/km': '4';
+  città: 'roma';
+  prezzo: '45';
+  marca: 'bmw';
+  modello: '320d';
+  categoria: 'berlina';
+  anno: '2016';
+}
---
+++ final-project-fe/src/app/interfaces/user.ts
@@ -0,0 +1,9 @@
+export interface User {
+  email: 'mario.rossi@gmail.com';
+  password: 'mario.rossi@gmail.com';
+  firstname: 'Mario';
+  lastname: 'Rossi';
+  age: '24';
+  citta: 'roma';
+  id: 1;
+}
