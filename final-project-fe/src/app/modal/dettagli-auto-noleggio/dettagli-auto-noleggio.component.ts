02e5994f03b158ee92c500b45a06f18a8bb848e6
gigijeskai <luigindaco@gmail.com>
2022-11-26 15:46:28 +0100

    modal

--- final-project-fe/src/app/modal/dettagli-auto-noleggio/dettagli-auto-noleggio.component.ts
+++ final-project-fe/src/app/modal/dettagli-auto-noleggio/dettagli-auto-noleggio.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Announcement } from 'src/app/interfaces/announcement';
+import { AnnouncementService } from 'src/app/services/announcementSrv';
 
 @Component({
   selector: 'app-dettagli-auto-noleggio',
   templateUrl: './dettagli-auto-noleggio.component.html',
-  styleUrls: ['./dettagli-auto-noleggio.component.scss']
+  styleUrls: ['./dettagli-auto-noleggio.component.scss'],
 })
 export class DettagliAutoNoleggioComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private announcementSrv: AnnouncementService) {}
+  announcements?: Announcement[];
   ngOnInit(): void {
+    this.announcementSrv.getAnnouncement().subscribe((data) => {
+      this.announcementSrv.announcements = data;
+      this.announcements = data;
+    });
   }
-
 }
--- final-project-fe/src/app/modal/modal.module.ts
+++ final-project-fe/src/app/modal/modal.module.ts
@@ -2,14 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DettagliAutoNoleggioComponent } from './dettagli-auto-noleggio/dettagli-auto-noleggio.component';
 
-
-
 @NgModule({
-  declarations: [
-    DettagliAutoNoleggioComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [DettagliAutoNoleggioComponent],
+  imports: [CommonModule],
+  exports: [DettagliAutoNoleggioComponent],
 })
-export class ModalModule { }
+export class ModalModule {}
