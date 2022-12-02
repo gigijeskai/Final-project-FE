ff6fb8f0c16dd0ab25dca7f78c5a486ef4fa8a35
gigijeskai <luigindaco@gmail.com>
2022-12-02 16:42:48 +0100

    fix card announcements

--- final-project-fe/src/app/pages/announcement/announcement.page.ts
+++ final-project-fe/src/app/pages/announcement/announcement.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
 import { Announcement } from 'src/app/interfaces/announcement';
 import { AnnouncementService } from 'src/app/services/announcementSrv';
 
@@ -7,12 +8,14 @@ import { AnnouncementService } from 'src/app/services/announcementSrv';
   styleUrls: ['./announcement.page.scss'],
 })
 export class AnnouncementPage implements OnInit {
-  constructor(private announcementSrv: AnnouncementService) {}
+  constructor(
+    private announcementSrv: AnnouncementService,
+    public authService: AuthService
+  ) {}
   announcements?: Announcement[];
   carDetails?: Announcement;
   searchText: string = '';
   isAlert = false;
-  item?: number;
 
   ngOnInit(): void {
     this.announcementSrv.getAnnouncement().subscribe((data) => {
