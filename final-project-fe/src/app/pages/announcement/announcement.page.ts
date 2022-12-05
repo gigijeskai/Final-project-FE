8963f16d9f59da63481687f4ef927660e9bb80ab
gigijeskai <luigindaco@gmail.com>
2022-12-05 13:07:03 +0100

    adding stripe

--- final-project-fe/src/app/pages/announcement/announcement.page.ts
+++ final-project-fe/src/app/pages/announcement/announcement.page.ts
@@ -1,3 +1,4 @@
+import { isNgTemplate } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/auth/auth.service';
 import { Announcement } from 'src/app/interfaces/announcement';
@@ -16,6 +17,7 @@ export class AnnouncementPage implements OnInit {
   carDetails?: Announcement;
   searchText: string = '';
   isAlert = false;
+  handler: any = null;
 
   ngOnInit(): void {
     this.announcementSrv.getAnnouncement().subscribe((data) => {
@@ -30,4 +32,45 @@ export class AnnouncementPage implements OnInit {
   onSearchTextEntered(searchValue: string) {
     this.searchText = searchValue;
   }
+  pay(prezzo: Announcement) {
+    var handler = (<any>window).StripeCheckout.configure({
+      key: 'pk_test_51MBb5SH7ocw78Ezlyf2imJT8xBmMBbMY7c8jVTRb1EBrtAPjkkPjbDxmlSl6dXyKTurtxsZLonH16N9MnGistTMP00Lf6vyuOy',
+      locale: 'auto',
+      token: function (token: any) {
+        // You can access the token ID with `token.id`.
+        // Get the token ID to your server-side code for use.
+
+        alert("Pagamento eseguito con successo, grazie per l'acquisto");
+      },
+    });
+
+    handler.open({
+      name: 'Demo Site',
+      description: 'Pagamento Stripe',
+      amount: prezzo,
+    });
+  }
+
+  // loadStripe() {
+  //   if (!window.document.getElementById('stripe-script')) {
+  //     var s = window.document.createElement('script');
+  //     s.id = 'stripe-script';
+  //     s.type = 'text/javascript';
+  //     s.src = 'https://checkout.stripe.com/checkout.js';
+  //     s.onload = () => {
+  //       this.handler = (<any>window).StripeCheckout.configure({
+  //         key: 'pk_test_51MBb5SH7ocw78Ezlyf2imJT8xBmMBbMY7c8jVTRb1EBrtAPjkkPjbDxmlSl6dXyKTurtxsZLonH16N9MnGistTMP00Lf6vyuOy',
+  //         locale: 'auto',
+  //         token: function (token: any) {
+  //           // You can access the token ID with `token.id`.
+  //           // Get the token ID to your server-side code for use.
+
+  //           alert('Payment Success!!');
+  //         },
+  //       });
+  //     };
+
+  //     window.document.body.appendChild(s);
+  //   }
+  // }
 }
