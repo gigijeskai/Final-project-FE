42ec80cfba8e9484c6c0c702cedf408566bf0ac4
gigijeskai <luigindaco@gmail.com>
2022-12-08 12:06:49 +0100

    delete e update method mongodb

--- node-express-mongodb/controllers/cars.js
+++ node-express-mongodb/controllers/cars.js
@@ -1,11 +1,11 @@
-import { v4 as uuidv4 } from "uuid";
+import { mongoose } from "mongoose";
 import { Car } from "../models/car.js";
+
 let cars = [];
 
 export const getAllCars = async (req, res) => {
   try {
     const cars = await Car.find();
-
     res.status(200).json(cars);
   } catch (error) {
     res.status(404).json({ message: error.message });
@@ -13,45 +13,42 @@ export const getAllCars = async (req, res) => {
 };
 export const getCarByID = async (req, res) => {
   const { id } = req.params;
-
+  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).json({ message: "id non idoneo" });
   try {
     const car = await Car.findById(id);
-
     res.status(200).json(car);
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
 };
-
 export const insertCar = async (req, res) => {
   const car = req.body;
   const newCar = new Car(car);
-
   try {
     await newCar.save();
-
     res.satus(201).json(newCar);
   } catch (error) {
     res.status(409).json({ message: error.message });
   }
 };
-
-export const deleteCar = (req, res) => {
+export const deleteCar = async (req, res) => {
   const { id } = req.params;
-  cars = cars.filter((car) => car.id != id);
-  res.send(`auto con id ${id} è stata rimossa `);
+  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).json({ message: "id non idoneo" });
+  try {
+    await Car.findByIdAndDelete(id);
+    res.status(200).json({ message: "car eliminata con successo" });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 };
-export const updateCar = (req, res) => {
+export const updateCar = async (req, res) => {
   const { id } = req.params;
-  const { marca, modello, prezzo } = req.body;
-
-  const carFind = cars.find((car) => car.id == id);
-
-  if (marca) carFind.marca = marca;
-
-  if (modello) carFind.marca = modello;
-
-  if (prezzo) carFind.marca = prezzo;
-
-  res.send(`auto con id ${id} è stata modificata `);
+  const data = { ...req.body };
+  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).json({ message: "id non idoneo" });
+  try {
+    const car = await Car.findByIdAndUpdate(id, data);
+    res.status(200).json(car);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 };
