33c8f8c3cfc6293479710a6aeecac9ee89e182a0
gigijeskai <luigindaco@gmail.com>
2022-12-16 12:42:51 +0100

    commit

--- final-project-fe/src/app/pages/home/home.module.ts
+++ final-project-fe/src/app/pages/home/home.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePage } from './home.page';
 import { ComponentsModule } from 'src/app/components/components.module';
-import { NgbdCarouselPause } from '../../components/carousel/carousel.component';
 
 @NgModule({
   declarations: [HomePage],
-  imports: [CommonModule, ComponentsModule, NgbdCarouselPause],
+  imports: [CommonModule, ComponentsModule],
 })
 export class HomeModule {}
